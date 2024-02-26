9945ab6af658c30808a26ee81ba020786166d63c
therealkingcob <rishiashah19@gmail.com>
2024-02-25 18:38:14 -0800

    set up color changing for the divs

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const container = document.querySelector('#container');
 for (i = 0; i < 256; i++) {
     const div = document.createElement('div');
     div.setAttribute('id', 'div');  
+    div.addEventListener('mouseenter', function() {
+            div.setAttribute('style', 'background-color: blue;');    
+      });
     container.appendChild(div);
 }
